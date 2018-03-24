d3e15c33724bf7cfe612ac9cef50c16afa37dc85
Killumin <christian.b1998@web.de>
2018-03-25 00:36:50 +0100

    Sooo

--- PLS/src/Main.java
+++ PLS/src/Main.java
@@ -1,6 +1,10 @@
+import javax.swing.JFrame;
 
 public class Main {
   public static void main(String[] args) {
 	  System.out.println("Hello there!");
+	  JFrame meinFrame = new JFrame("Beispiel JFrame");
+	  meinFrame.setSize(400,400);
+      meinFrame.setVisible(true);
   }
 }
